8eaeed2d0f1df2641b69b77581bcae90053ed324
alikaderrg <alikader.rg@gmail.com>
2025-05-01 19:31:14 +0100

    updated backend

--- cod-backend/server.js
+++ cod-backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/shopify', shopifyRoutes);
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // Routes
