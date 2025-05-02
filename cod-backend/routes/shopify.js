3c606a77e0526a345aa540238ab0d3a37717a285
alikaderrg <alikader.rg@gmail.com>
2025-05-02 23:15:42 +0100

    update v0.0.10

--- cod-backend/routes/shopify.js
+++ cod-backend/routes/shopify.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { testShopifyConnection, saveShopifyCredentials } from '../controllers/shopifyController.js';
+import { testShopifyConnection } from '../controllers/shopifyController.js';
+import { saveShopifyCredentials } from '../controllers/shopifySaveController.js';
 import { verifyToken } from '../middlewares/authenticate.js';
 
 const router = express.Router();
