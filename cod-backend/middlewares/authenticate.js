f1125ed1a9a5b9a3ecf4505e8c132300952f6722
alikaderrg <alikader.rg@gmail.com>
2025-05-03 00:08:35 +0100

    update v0.0.14

--- cod-backend/middlewares/authenticate.js
+++ cod-backend/middlewares/authenticate.js
@@ -5,14 +5,15 @@ export const authenticateToken = (req, res, next) => {
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ error: 'Access denied. No token provided.' });
+    return res.status(401).json({ message: 'Access token missing' });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return res.status(403).json({ error: 'Invalid token.' });
+      return res.status(403).json({ message: 'Invalid token' });
     }
-    req.user = decoded;
+
+    req.user = user;
     next();
   });
 };
