13eab3ddad68d0304fe1c515b8303366baacc1d2
alikaderrg <alikader.rg@gmail.com>
2025-05-09 11:51:47 +0100

    Update Signup/Login - V0.0.35

---
+++ cod-backend/prisma/migrations/20250509103525_add_username/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `passwordHash` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `fullName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `phoneNumber` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `storeName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+DROP COLUMN "passwordHash",
+ADD COLUMN     "fullName" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "phoneNumber" TEXT NOT NULL,
+ADD COLUMN     "storeName" TEXT NOT NULL,
+ADD COLUMN     "username" TEXT NOT NULL,
+ALTER COLUMN "id" DROP DEFAULT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
